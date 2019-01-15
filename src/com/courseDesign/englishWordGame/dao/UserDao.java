ead6144c20b1d4c23ff3bc610419455db14c8a5e
Unknown <1018729292@qq.com>
2019-01-15 22:45:56 +0800

    优化相同积分为并列排名

--- src/com/courseDesign/englishWordGame/dao/UserDao.java
+++ src/com/courseDesign/englishWordGame/dao/UserDao.java
@@ -46,7 +46,7 @@ public class UserDao {
             //1.得到一个连接
             Connection conn = DBUtil.getConn();
             //2.得到操作数据库对象
-            String sql = "SELECT(@rank := @rank+1) AS 累计积分排名,a.* FROM(SELECT * FROM 用户)a,(SELECT @rank :=0)b ORDER BY a." + difficulty + " DESC";
+            String sql = "SELECT CASE WHEN @prevRank = " + difficulty + " THEN @rank WHEN @prevRank := " + difficulty + " THEN @rank := @rank+1 END AS 累计积分排名,a.* FROM(SELECT * FROM 用户)a,(SELECT @rank :=0,@prevRank :=NULL)b ORDER BY a." + difficulty + " DESC";
             PreparedStatement stmt = conn.prepareStatement(sql);
             ResultSet rs = stmt.executeQuery();//执行
             List<User> list = new ArrayList<User>();
