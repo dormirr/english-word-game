ca734c82abe2373958012e38c0d263c5c4c3ab28
Unknown <1018729292@qq.com>
2019-01-15 21:57:42 +0800

    优化计时的精度

--- web/playassets/js/time.js
+++ web/playassets/js/time.js
@@ -1,9 +1,10 @@
 //为a标签注册onclick事件
 //按秒计算
-var maxtime = 5;
+var maxtime = 6;
 var sum = document.getElementById('sum').value;
 var id = document.getElementById('id').value;
 var Difficulty = document.getElementById('Difficulty').value;
+var tid = document.getElementById('tid').value;
 
 function CountDown() {
     if (maxtime >= 0) {
@@ -11,7 +12,7 @@ function CountDown() {
         document.all["timer"].innerHTML = msg;
         --maxtime;
     } else {
-        window.location.href = 'playServlet?type=judge&&id=' + id + '&&fid=0&&tid=1&&sum=' + sum + '&&Difficulty=' + Difficulty;
+        window.location.href = 'playServlet?type=judge&&id=' + id + '&&fid=0&&tid=' + tid + '&&sum=' + sum + '&&Difficulty=' + Difficulty;
     }
     timer = setTimeout("CountDown()", 1000);
 }
